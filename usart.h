a37998712a13918399c1445d22e0c6918be76e1e
Rustam <92742845+feblepy@users.noreply.github.com>
2023-02-12 20:45:05 +0400

    Delete usart.h

--- usart.h
+++
@@ -1,9 +0,0 @@
-#ifndef USART_H_
-#define USART_H_
-
-#include "global_define.h"
-void USART_init(uint16_t);
-void USART_putchar(uint8_t);
-void USART_putstring(uint8_t*);
-void USART_putsymbol(uint32_t);
-#endif
